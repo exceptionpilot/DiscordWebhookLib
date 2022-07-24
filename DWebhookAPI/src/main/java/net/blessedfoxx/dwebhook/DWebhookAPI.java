76653e6395df276f99867a6f2420facec3692297
BlessedFoxX <85083169+BlessedFoxX@users.noreply.github.com>
2022-07-24 21:41:10 +0200

    Update DWebhookAPI.java

--- DWebhookAPI/src/main/java/net/blessedfoxx/dwebhook/DWebhookAPI.java
+++ DWebhookAPI/src/main/java/net/blessedfoxx/dwebhook/DWebhookAPI.java
@@ -9,7 +9,7 @@ public class DWebhookAPI {
         /*
         @example : here u see how to use this small api :)
          */
-        DWebhookMessage webhookMessage = new DWebhookMessage("https://discord.com/api/webhooks/1000756476903706644/pWP4k843rsrj6HuengJYTLTXwYNY32DmjNYFFRK8dKArAeRQKmAHa6vk0WRijGjdkuen");
+        DWebhookMessage webhookMessage = new DWebhookMessage("your webhook");
         webhookMessage.setAvatar("https://cdn.discordapp.com/avatars/656913610584358935/a_af37918b9202106c1a5dcbbdeb0ae1a5.gif?size=512");
         webhookMessage.setUsername("DWebhook by Angekotzter");
         webhookMessage.setMessageContent("Version: 1.0");
